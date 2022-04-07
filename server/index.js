329f7a1ad86cc8fce4939a8d394355524cf89caf
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-04-07 17:45:47 +0200

    server connected to database

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+dotenv.config();
+const CONNECTION_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error));
