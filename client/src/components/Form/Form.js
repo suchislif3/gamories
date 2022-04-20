29cb2ed8cc9aa4eccb831841858cd7432e1175d6
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-04-20 16:53:02 +0200

    made Form component and added basic apis

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,8 +1,95 @@
+import { useState } from "react";
+import { Button, Paper, TextField, Typography } from "@material-ui/core";
+import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+
 import useStyles from "./styles";
+import { createPost } from "../../actions/postsAction";
 
 const Form = () => {
   const classes = useStyles();
-  return <h1>Form</h1>;
+  const [postData, setPostData] = useState({
+    title: "",
+    description: "",
+    tags: "",
+    selectedFile: "",
+  });
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createPost(postData))
+  };
+  const clear = () => {};
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Create a Gamory</Typography>
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="description"
+          variant="outlined"
+          label="Description"
+          fullWidth
+          multiline
+          minRows={2}
+          maxRows={5}
+          value={postData.description}
+          onChange={(e) =>
+            setPostData({ ...postData, description: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="contained"
+          className={classes.buttonSubmit}
+          color="primary"
+          size="large"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          onClick={clear}
+          variant="contained"
+          color="secondary"
+          size="small"
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
