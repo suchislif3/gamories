54e1957939159d5c2483f19329982b7c21d12a6c
Szabolcs Devecseri <sz.devecseri@gmail.com>
2022-05-12 16:30:21 +0200

    disable react and redux devtools for production

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import App from "./App";
 import store from "./store/store";
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
